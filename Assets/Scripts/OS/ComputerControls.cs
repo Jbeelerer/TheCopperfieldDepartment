949e7e6e01ff1007749b64581ec1cd46dfb5c1da
haebel7 <ahaeberlin7@gmx.ch>
2024-03-19 00:12:48 +0100

    OS multiple windows, functionality to bring to foreground

--- Assets/Scripts/OS/ComputerControls.cs
+++ Assets/Scripts/OS/ComputerControls.cs
@@ -2,51 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Device;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public enum OSAppType
+{
+    SOCIAL,
+    GOV,
+    SETTINGS
+}
 
 public class ComputerControls : MonoBehaviour
 {
     public float speed = 1;
     public RectTransform cursor;
     public OSWindow testWindow;
+    public GameObject windowPrefab;
+    public List<OSApplication> apps = new List<OSApplication>();
 
     private RectTransform screen;
     private float h;
     private float v;
+    private List<OSWindow> windows = new List<OSWindow>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
 
         screen = GetComponent<RectTransform>();
-        cursor.anchoredPosition += new Vector2(0, 15 * speed);
+        windows.Add(testWindow);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*Vector3 point = new Vector3();
-        Vector2 mousePos = new Vector2();
-        Vector2 newPos = new Vector2();
-        mousePos = Input.mousePosition;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(this.GetComponent<RectTransform>(), mousePos, cam, out newPos);
-        //point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-        cursor.anchoredPosition = newPos;*/
-
         h = speed * Input.GetAxis("Mouse X");
         v = speed * Input.GetAxis("Mouse Y");
 
         if (Input.GetMouseButtonDown(0))
         {
-            //print(cursor.anchoredPosition);
-            if (RectsOverlapping(cursor, testWindow.topBar))
-            {
-                testWindow.isMoving = true;
-            }
+            // Clicked window bar
+            CheckWindowClicked();
+            cursor.transform.SetAsLastSibling();
         }
+
         if (Input.GetMouseButtonUp(0))
         {
-            testWindow.isMoving = false;
+            foreach (OSWindow window in windows)
+            {
+                window.isMoving = false;
+            }
+
+            // Clicked an app
+            CheckAppClicked();
+            cursor.transform.SetAsLastSibling();
         }
     }
 
@@ -54,41 +65,94 @@ public class ComputerControls : MonoBehaviour
     {
         cursor.anchoredPosition += new Vector2(h, v);
 
-        if (testWindow.isMoving)
+        foreach (OSWindow window in windows)
         {
-            testWindow.MoveWindow(new Vector2(h, v));
+            if (window.isMoving)
+            {
+                window.MoveWindow(new Vector2(h, v));
+            }
         }
 
         // Reverse the movement if the cursor went outside the screen
-        if (!RectsOverlapping(cursor, screen))
+        if (!PointInsideRect(cursor.position, screen))
         {
             cursor.anchoredPosition -= new Vector2(h, v);
 
-            if (testWindow.isMoving)
+            foreach (OSWindow window in windows)
             {
-                testWindow.MoveWindow(-(new Vector2(h, v)));
+                if (window.isMoving)
+                {
+                    window.MoveWindow(-(new Vector2(h, v)));
+                }
             }
         }
     }
 
-    private bool RectsOverlapping(RectTransform firstRect, RectTransform secondRect)
+    private void CheckWindowClicked()
     {
-        if (firstRect.position.x + firstRect.transform.TransformVector(firstRect.rect.size).x * 0.5f < secondRect.position.x - secondRect.transform.TransformVector(secondRect.rect.size).x * 0.5f)
+        foreach (OSWindow window in windows)
         {
-            return false;
+            if (PointInsideRect(cursor.position, window.topBar))
+            {
+                // Check if window is behind another element
+                if (TargetIsInFront(window.topBar.gameObject))
+                {
+                    window.isMoving = true;
+                    window.transform.SetAsLastSibling();
+                    return;
+                }
+            }
         }
-        if (secondRect.position.x + secondRect.transform.TransformVector(secondRect.rect.size).x * 0.5f < firstRect.position.x - firstRect.transform.TransformVector(firstRect.rect.size).x * 0.5f)
+    }
+
+    private void CheckAppClicked()
+    {
+        foreach (OSApplication app in apps)
         {
-            return false;
+            if (PointInsideRect(cursor.position, app.recTrans))
+            {
+                // Check if icon is behind another element
+                if (!TargetIsInFront(app.gameObject))
+                {
+                    return;
+                }
+                // Check if the window is already open
+                foreach (OSWindow window in windows)
+                {
+                    if (window.appType == app.appType)
+                    {
+                        return;
+                    }
+                }
+                GameObject newWindow = Instantiate(windowPrefab, transform.position, transform.rotation, transform);
+                newWindow.GetComponent<OSWindow>().appType = app.appType;
+                windows.Add(newWindow.GetComponent<OSWindow>());
+                return;
+            }
         }
-        if (firstRect.position.y + firstRect.transform.TransformVector(firstRect.rect.size).y * 0.5f < secondRect.position.y - secondRect.transform.TransformVector(secondRect.rect.size).y * 0.5f)
+    }
+
+    private bool TargetIsInFront(GameObject target)
+    {
+        GraphicRaycaster gr = this.GetComponent<GraphicRaycaster>();
+        PointerEventData ped = new PointerEventData(null);
+        ped.position = GetComponent<Canvas>().worldCamera.WorldToScreenPoint(cursor.position);
+        List<RaycastResult> results = new List<RaycastResult>();
+        gr.Raycast(ped, results);
+        if (Object.ReferenceEquals(results[0].gameObject, target))
         {
-            return false;
+            return true;
         }
-        if (secondRect.position.y + secondRect.transform.TransformVector(secondRect.rect.size).y * 0.5f < firstRect.position.y - firstRect.transform.TransformVector(firstRect.rect.size).y * 0.5f)
-        {
-            return false;
-        }
-        return true;
+        return false;
+    }
+
+    private bool PointInsideRect(Vector2 point, RectTransform rectTransform)
+    {
+        Rect rect = rectTransform.rect;
+        Vector2 rectMin = (Vector2)rectTransform.position - rectTransform.pivot * (Vector2)rectTransform.transform.TransformVector(rect.size);
+        Vector2 rectMax = rectMin + (Vector2)rectTransform.transform.TransformVector(rect.size);
+
+        return point.x >= rectMin.x && point.x <= rectMax.x &&
+               point.y >= rectMin.y && point.y <= rectMax.y;
     }
 }
--- Assets/Scripts/OS/OSWindow.cs
+++ Assets/Scripts/OS/OSWindow.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OSWindow : MonoBehaviour
 {
-    private RectTransform window;
-
+    [HideInInspector] public RectTransform rectTrans;
+    [HideInInspector] public OSAppType appType;
     [HideInInspector] public RectTransform topBar;
-    public bool isMoving = false;
+    [HideInInspector] public RectTransform buttonClose;
+    [HideInInspector] public bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        window = GetComponent<RectTransform>();
+        rectTrans = GetComponent<RectTransform>();
         topBar = transform.Find("TopBar").GetComponent<RectTransform>();
+        buttonClose = transform.Find("TopBar").Find("ButtonClose").GetComponent<RectTransform>();
+        transform.Find("Content").Find("Text").GetComponent<TextMeshProUGUI>().text = appType.ToString();
     }
 
-    // Update is called once per frame
     public void MoveWindow(Vector2 moveVec)
     {
-        window.anchoredPosition += moveVec;
+        rectTrans.anchoredPosition += moveVec;
     }
 }
