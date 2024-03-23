770918412d461e3afaec8e17af78c35ece2ac29c
haebel7 <60757873+haebel7@users.noreply.github.com>
2024-03-23 15:36:02 +0100

    ComputerControls script cleanup

--- Assets/Scripts/OS/ComputerControls.cs
+++ Assets/Scripts/OS/ComputerControls.cs
@@ -28,6 +28,8 @@ public class ComputerControls : MonoBehaviour
     private float h;
     private float v;
     private List<OSWindow> windows = new List<OSWindow>();
+    private OSWindow rightWindow;
+    private OSWindow leftWindow;
 
     // Start is called before the first frame update
     void Start()
@@ -105,109 +107,13 @@ public class ComputerControls : MonoBehaviour
         }
     }
 
-    private void CheckWindowMouseDown()
-    {
-        foreach (OSWindow window in windows)
-        {
-            if (PointInsideRect(cursor.position, window.topBar))
-            {
-                // Check if window is behind another element
-                if (TargetIsInFront(window.topBar.gameObject))
-                {
-                    // Start moving the affected window with the cursor
-                    window.isMoving = true;
-                    window.transform.SetAsLastSibling();
-
-                    // Reset window to small mode if moved while in another size mode
-                    if (!window.isInSmallMode) {
-                        ResizeWindowSmall(window);
-                        window.rectTrans.position = new Vector2(cursor.position.x, cursor.position.y - window.rectTrans.anchorMax.y);
-                    }
-
-                    return;
-                }
-            }
-        }
-    }
-
-    private void CheckWindowMouseUp()
-    {
-        foreach (OSWindow window in windows)
-        {
-            // Clicked buttonClose
-            if (PointInsideRect(cursor.position, window.buttonClose))
-            {
-                // Check if window is behind another element
-                if (TargetIsInFront(window.buttonClose.gameObject))
-                {
-                    windows.Remove(window);
-                    Destroy(window.gameObject);
-                    return;
-                }
-            }
-            // Clicked buttonSmall
-            else if (PointInsideRect(cursor.position, window.buttonSmall))
-            {
-                // Check if window is behind another element
-                if (TargetIsInFront(window.buttonSmall.gameObject))
-                {
-                    // Make window small
-                    ResizeWindowSmall(window);
-                    return;
-                }
-            }
-            // Clicked buttonLong
-            else if (PointInsideRect(cursor.position, window.buttonLong))
-            {
-                // Check if window is behind another element
-                if (TargetIsInFront(window.buttonLong.gameObject))
-                {
-                    // Make window long, position on right
-                    window.rectTrans.anchorMin = new Vector2(0.5f, 0);// new Vector2(screen.pivot.x, screen.anchorMin.y);
-                    window.rectTrans.anchorMax = new Vector2(1, 1);// screen.anchorMax;
-                    window.rectTrans.offsetMin = new Vector2(0, 0);
-                    window.rectTrans.offsetMax = new Vector2(0, 0);
-                    window.isInSmallMode = false;
-                    return;
-                }
-            }
-            // Clicked buttonBig
-            else if (PointInsideRect(cursor.position, window.buttonBig))
-            {
-                // Check if window is behind another element
-                if (TargetIsInFront(window.buttonBig.gameObject))
-                {
-                    // Make window fullscreen
-                    window.rectTrans.anchorMin = new Vector2(0, 0);
-                    window.rectTrans.anchorMax = new Vector2(1, 1);
-                    window.rectTrans.offsetMin = new Vector2(0, 0);
-                    window.rectTrans.offsetMax = new Vector2(0, 0);
-                    window.isInSmallMode = false;
-                    return;
-                }
-            }
-        }
-    }
-
-    private void ResizeWindowSmall(OSWindow window)
-    {
-        window.rectTrans.anchorMin = new Vector2(0.5f, 0.5f);
-        window.rectTrans.anchorMax = new Vector2(0.5f, 0.5f);
-        window.rectTrans.sizeDelta = new Vector2(450, 300);
-        window.isInSmallMode = true;
-    }
-
     private void CheckAppMouseUp()
     {
         foreach (OSApplication app in apps)
         {
-            if (PointInsideRect(cursor.position, app.recTrans))
+            GameObject hitObject = GetFirstHitObject();
+            if (Object.ReferenceEquals(hitObject, app.gameObject))
             {
-                // Check if icon is behind another element
-                if (!TargetIsInFront(app.gameObject))
-                {
-                    return;
-                }
                 // Check if the window is already open
                 foreach (OSWindow window in windows)
                 {
@@ -224,18 +130,124 @@ public class ComputerControls : MonoBehaviour
         }
     }
 
-    private bool TargetIsInFront(GameObject target)
+    private void CheckWindowMouseDown()
+    {
+        foreach (OSWindow window in windows)
+        {
+            if (PointInsideRect(cursor.position, window.rectTrans))
+            {
+                // Bring clicked window to front
+                window.transform.SetAsLastSibling();
+
+                // Check if topbar was hit
+                GameObject hitObject = GetFirstHitObject();
+                if (Object.ReferenceEquals(hitObject, window.topBar.gameObject))
+                {
+                    // Start moving the affected window with the cursor
+                    window.isMoving = true;
+
+                    // Reset window to small mode if moved while in another size mode
+                    if (!window.isInSmallMode)
+                    {
+                        ResizeWindowSmall(window);
+                        window.rectTrans.position = new Vector2(cursor.position.x, cursor.position.y - window.rectTrans.anchorMax.y);
+                    }
+
+                    return;
+                }
+            }
+        }
+    }
+
+    private void CheckWindowMouseUp()
+    {
+        foreach (OSWindow window in windows)
+        {
+            if (PointInsideRect(cursor.position, window.rectTrans))
+            {
+                // Check which element was hit
+                GameObject hitObject = GetFirstHitObject();
+
+                if (Object.ReferenceEquals(hitObject, window.buttonClose.gameObject))
+                {
+                    // Close window
+                    RemoveLeftRightWindow(window);
+                    windows.Remove(window);
+                    Destroy(window.gameObject);
+                    return;
+                }
+                else if (Object.ReferenceEquals(hitObject, window.buttonSmall.gameObject))
+                {
+                    // Make window small
+                    RemoveLeftRightWindow(window);
+                    ResizeWindowSmall(window);
+                    return;
+                }
+                else if (Object.ReferenceEquals(hitObject, window.buttonLong.gameObject))
+                {
+                    // Make window long, position on left or right
+                    if (!leftWindow)
+                    {
+                        window.rectTrans.anchorMin = new Vector2(0, 0);// new Vector2(screen.pivot.x, screen.anchorMin.y);
+                        window.rectTrans.anchorMax = new Vector2(0.5f, 1);// screen.anchorMax;
+                        window.rectTrans.offsetMin = new Vector2(0, 0);
+                        window.rectTrans.offsetMax = new Vector2(0, 0);
+                        leftWindow = window;
+                    }
+                    else
+                    {
+                        window.rectTrans.anchorMin = new Vector2(0.5f, 0);
+                        window.rectTrans.anchorMax = new Vector2(1, 1);
+                        window.rectTrans.offsetMin = new Vector2(0, 0);
+                        window.rectTrans.offsetMax = new Vector2(0, 0);
+                        rightWindow = window;
+                    }
+                    window.isInSmallMode = false;
+                    return;
+                }
+                else if (Object.ReferenceEquals(hitObject, window.buttonBig.gameObject))
+                {
+                    // Make window fullscreen
+                    RemoveLeftRightWindow(window);
+
+                    window.rectTrans.anchorMin = new Vector2(0, 0);
+                    window.rectTrans.anchorMax = new Vector2(1, 1);
+                    window.rectTrans.offsetMin = new Vector2(0, 0);
+                    window.rectTrans.offsetMax = new Vector2(0, 0);
+                    window.isInSmallMode = false;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void RemoveLeftRightWindow(OSWindow window)
+    {
+        if (Object.ReferenceEquals(window, leftWindow))
+        {
+            leftWindow = null;
+        } else if (Object.ReferenceEquals(window, rightWindow))
+        {
+            rightWindow = null;
+        }
+    }
+
+    private void ResizeWindowSmall(OSWindow window)
+    {
+        window.rectTrans.anchorMin = new Vector2(0.5f, 0.5f);
+        window.rectTrans.anchorMax = new Vector2(0.5f, 0.5f);
+        window.rectTrans.sizeDelta = new Vector2(450, 300);
+        window.isInSmallMode = true;
+    }
+
+    private GameObject GetFirstHitObject()
     {
         GraphicRaycaster gr = this.GetComponent<GraphicRaycaster>();
         PointerEventData ped = new PointerEventData(null);
         ped.position = GetComponent<Canvas>().worldCamera.WorldToScreenPoint(cursor.position);
         List<RaycastResult> results = new List<RaycastResult>();
         gr.Raycast(ped, results);
-        if (Object.ReferenceEquals(results[0].gameObject, target))
-        {
-            return true;
-        }
-        return false;
+        return results[0].gameObject;
     }
 
     private bool PointInsideRect(Vector2 point, RectTransform rectTransform)
