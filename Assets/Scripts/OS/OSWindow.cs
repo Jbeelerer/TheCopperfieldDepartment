d7ff456033d0b863dc085af11ad1517b7281b928
Jbeelerer <91538755+Jbeelerer@users.noreply.github.com>
2025-03-02 10:45:06 +0100

    test to fix mainscene

--- Assets/Scripts/OS/OSWindow.cs
+++ Assets/Scripts/OS/OSWindow.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.PackageManager.UI;
+//using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
