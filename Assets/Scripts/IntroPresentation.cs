e6cba6e5b7f6f48297aa378f17288ac2c4a6fe92
haebel7 <60757873+haebel7@users.noreply.github.com>
2025-08-22 13:41:45 +0200

    Started on intro presentation script

---
+++ Assets/Scripts/IntroPresentation.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroPresentation : MonoBehaviour
+{
+
+    [SerializeField] private Sprite[] slides;
+    [SerializeField] private Image slideImage;
+    [SerializeField] private GameObject mousePrompt;
+    [SerializeField] private GameObject blackScreen;
+
+    private Narration narration;
+
+    private int currentSlideIndex = -1;
+
+    void Start()
+    {
+        narration = FindObjectOfType<Narration>();
+
+        slideImage.sprite = slides[0];
+        mousePrompt.SetActive(false);
+
+        ShowNextSlide();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && mousePrompt.activeInHierarchy)
+        {
+            ShowNextSlide();
+            ShowMousePromptDelayed();
+        }
+    }
+
+    private void ShowNextSlide()
+    {
+        currentSlideIndex++;
+        //narration.PlaySequence("");
+        slideImage.sprite = slides[currentSlideIndex];
+        mousePrompt.SetActive(false);
+        // TODO: Temporary fix, mouseprompt should be triggered when narration ends
+        ShowMousePromptDelayed();
+    }
+
+    public void ShowMousePrompt()
+    {
+        mousePrompt.SetActive(true);
+    }
+
+    private IEnumerator ShowMousePromptDelayed()
+    {
+        yield return new WaitForSeconds(3f);
+        ShowMousePrompt();
+    }
+}
