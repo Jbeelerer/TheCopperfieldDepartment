b162f068e39a3a81a94e5e22450ec7e2bca89783
Jbeelerer <91538755+Jbeelerer@users.noreply.github.com>
2026-01-03 11:10:55 +0100

    Added Feedbackform

---
+++ Assets/Scripts/FeedbackForm.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class FeedbackForm : MonoBehaviour
+{
+    [SerializeField] GameObject form;
+    [SerializeField] GameObject success;
+    private int rating = 0;
+    [SerializeField] TMP_InputField liked ;
+    [SerializeField] TMP_InputField disliked;  
+    static string url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfCG5pp32DBLsfTYhQ2MIVmU1Scle8FtYXlEd_9nbGHAk5Z8A/formResponse";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetRating(int rating)
+    {
+     rating = int.Parse(rating.ToString());   
+    }
+    public void Send()
+    {
+        StartCoroutine(SendFeedback(rating, disliked.text, liked.text));
+        form.SetActive(false);
+        success.SetActive(true);
+    }
+    IEnumerator SendFeedback(int rating, string disliked, string liked)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1328074360", rating);
+        form.AddField("entry.107984603", liked);
+        form.AddField("entry.1174536770", disliked);
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+        yield return www.SendWebRequest();
+    }
+}
